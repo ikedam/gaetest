228725ca95d75306bf8dedf6ce094f7ed5023dd1
ikedam <devld@ikedam.jp>
2017-08-18 14:40:08 +0900

    Make unittests pass

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,4 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -7,6 +10,10 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        NgbModule,
+        RouterTestingModule.withRoutes([]),
       ],
     }).compileComponents();
   }));
@@ -17,16 +24,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'gaetest'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('gaetest');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a .navbar-brand', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('.navbar-brand').textContent).toContain('gaetest');
   }));
 });
