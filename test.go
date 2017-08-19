b3d7241612b33556d5ce88df05b943e02f2ecae0
ikedam <devld@ikedam.jp>
2017-08-19 22:34:12 +0900

    Update test.go to run e2e tests

--- test.go
+++ test.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 	"syscall"
+	"time"
 )
 
 func runCommand(name string, args... string) int {
+	fmt.Fprintf(os.Stderr, "# %v %v\n", name, strings.Join(args, " "))
 	cmd := exec.Command(name, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -23,6 +28,102 @@ func runCommand(name string, args... string) int {
 	return 0
 }
 
+func shutdownServer(cmd *exec.Cmd) bool {
+	if resp, err := http.Get("http://localhost:8000/quit"); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to stop server: %v", err)
+		return false
+	} else if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderr, "Failed to stop server: %v", resp)
+		return false
+	}
+	return true
+}
+
+func runE2ETest() int {
+	serverStdout, err := os.OpenFile(
+		"server.stdout.log",
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer serverStdout.Close()
+	serverStderr, err := os.OpenFile(
+		"server.stderr.log",
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer serverStderr.Close()
+
+	python, err := exec.LookPath("python")
+	if err != nil {
+		panic(err)
+	}
+	devAppServer, err := exec.LookPath("dev_appserver.py")
+	if err != nil {
+		panic(err)
+	}
+	args := []string {
+		devAppServer,
+		"--enable_watching_go_path=false",
+		"--clear_datastore=true",
+		"--datastore_consistency_policy=consistent",
+		"--automatic_restart=false",
+		"--watcher_ignore_re=.*",
+		"--skip_sdk_update_check=true",
+		"server/app.yaml",
+	}
+
+	fmt.Fprintf(os.Stderr, "# %v %v\n", python, strings.Join(args, " "))
+	cmd := exec.Command(python, args...)
+	cmd.Stdout = serverStdout
+	cmd.Stderr = serverStderr
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+	defer shutdownServer(cmd)
+
+	url := "http://localhost:8080/"
+	seconds := 30
+	fmt.Fprintf(os.Stderr, "Wait for server gets ready for %v seconds: %v\n", seconds, url)
+	limit := time.Now().Add(time.Duration(seconds) * time.Second)
+	client := &http.Client {
+		Timeout: time.Duration(seconds) * time.Second,
+	}
+	for true {
+		if time.Since(limit) > 0 {
+			panic(fmt.Sprintf("Server didnot get ready after %v seconds", seconds))
+		}
+		if _, err := client.Get(url); err == nil {
+			break
+		}
+		time.Sleep(time.Duration(100) * time.Millisecond)
+	}
+
+	if err := os.Chdir("client"); err != nil {
+		panic(err)
+	}
+
+	if ret := runCommand("yarn"); ret != 0 {
+		return ret
+	}
+
+	return runCommand(
+		"yarn",
+		"run",
+		"e2e",
+		"--",
+		"--serve",
+		"true",
+		"--port",
+		"4200",
+	)
+}
+
 func main() {
 	gopath := os.Getenv("GOPATH")
 	projectDir := path.Join(gopath, "src", "github.com", "ikedam", "gaetest")
@@ -32,5 +133,11 @@ func main() {
 	if ret := runCommand("glide", "install"); ret != 0 {
 		os.Exit(ret)
 	}
-	os.Exit(runCommand("goapp", "test", "./server", "./testutil"))
+	if ret := runCommand("goapp", "test", "./server", "./testutil"); ret != 0 {
+		os.Exit(ret)
+	}
+	if ret := runE2ETest(); ret != 0 {
+		os.Exit(ret)
+	}
+	os.Exit(0)
 }
