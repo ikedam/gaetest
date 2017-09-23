643598d6bbe1749eeaf842800fb267fde70d349d
ikedam <devld@ikedam.jp>
2017-09-24 01:45:51 +0900

    Add coverage feature

--- Dockerfile
+++ Dockerfile
@@ -1,40 +1,42 @@
 # https://cloud.google.com/appengine/docs/standard/go/download?hl=ja
 
-# alpine �� glibc ���Ȃ����߁A nodejs �� chrome-webdriver �����삵�Ȃ�
+# alpine は glibc がないため、 nodejs の chrome-webdriver が動作しない
 FROM ubuntu:latest
 
-ENV GOAPP_VERSION 1.9.57
-ENV GOAPP_URL https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-${GOAPP_VERSION}.zip
-ENV NODEJS_VERSION 6.11.2
-ENV NODEJS_URL https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-x64.tar.xz
-
-RUN echo "Installing packages..."
-
-# for add-apt-repository, https://...
+## Packages provided by the distribution
+# build-essential is especially heavy.
 RUN apt-get -y update \
 	&& apt-get install --no-install-recommends -y \
-	software-properties-common \
-	apt-transport-https
+	build-essential
 
-RUN echo "Preparing go-appengine..."
-RUN add-apt-repository ppa:masterminds/glide \
-	&& apt-get -y update \
+# Packages for go (go_appengine, glide)
+RUN apt-get -y update \
 	&& apt-get install --no-install-recommends -y \
 	build-essential \
 	python2.7 \
 	git \
-	curl \
 	golang \
-	glide \
-	unzip \
 	&& update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
 
-RUN curl -o go_appengine.zip ${GOAPP_URL} \
-	&& unzip -qd /usr/local go_appengine.zip \
-	&& rm -f go_appengine.zip
+# Packages for e2e
+RUN apt-get -y update \
+	&& apt-get install --no-install-recommends -y \
+	chromium-browser \
+	libgconf-2-4
 
+# utilities, add-apt-repository, support https://... by apt-get
+RUN apt-get -y update \
+	&& apt-get install --no-install-recommends -y \
+	curl \
+	unzip \
+	software-properties-common \
+	apt-transport-https
 
-RUN echo "Preparing angular..."
+## external packages
+# nodejs & yarn
+ARG NODEJS_VERSION="6.11.2"
+ARG NODEJS_URL="https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-x64.tar.xz"
+
 RUN curl -o node.tar.xz ${NODEJS_URL} \
 	&& mkdir /usr/local/nodejs \
 	&& tar xJ -C /usr/local/nodejs -f node.tar.xz --strip 1 \
@@ -46,12 +48,18 @@ RUN apt-get -y update \
 	&& apt-get install --no-install-recommends -y \
 	yarn
 
-RUN echo "Preparing e2e..."
-
-RUN apt-get -y update \
+# appengine
+RUN add-apt-repository ppa:masterminds/glide \
+	&& apt-get -y update \
 	&& apt-get install --no-install-recommends -y \
-	chromium-browser \
-	libgconf-2-4
+	glide
+
+ARG GOAPP_VERSION="1.9.58"
+ARG GOAPP_URL="https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-${GOAPP_VERSION}.zip"
+
+RUN curl -o go_appengine.zip ${GOAPP_URL} \
+	&& unzip -qd /usr/local go_appengine.zip \
+	&& rm -f go_appengine.zip
 
 RUN rm /var/lib/apt/lists/*_*
 
@@ -60,6 +68,21 @@ ENV GOPATH /go
 ENV PATH ${PATH}:/usr/local/go_appengine:/usr/local/nodejs/bin:${GOPATH}/bin
 ENV CHROME_BIN /usr/bin/chromium-browser
 
+# glide, yarn で使うキャッシュディレクトリ
+# docker volume create cache
+# docker run -v cache:/cache ...
+# とすればライブラリのセットアップ処理を高速化できます。
+RUN mkdir /cache
+
+ENV GLIDE_HOME /cache/glide
+ENV YARN_CACHE_FOLDER /cache/yarn
+
+# ログ出力
+# docker run -v `pwd`:/log ...
+# とすればログをカレントディレクトリに保存できます。
+RUN mkdir /log
+
 ADD . /go/src/github.com/ikedam/gaetest
 
-CMD ["go", "run", "/go/src/github.com/ikedam/gaetest/tool/test.go", "--docker"]
+ENTRYPOINT ["go", "run", "/go/src/github.com/ikedam/gaetest/tool/test.go", "--docker", "--log=/log"]
+#CMD ["go", "run", "/go/src/github.com/ikedam/gaetest/tool/test.go", "--docker", "--log=/log"]
--- tool/test.go
+++ tool/test.go
@@ -1,51 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"sort"
 	"strings"
 	"syscall"
 	"time"
 )
 
-type testOption struct{
-	IsVerbose bool
-	IsDocker bool
+type testOption struct {
+	IsVerbose  bool
+	IsDocker   bool
+	IsCoverage bool
+	LogDir     string
 }
 
-type testCommandInfo struct{
-	Command string
-	Func func(testOption) int
-	NeedGlide bool
-	NeedYarn bool
+type testCommandInfo struct {
+	Command     string
+	Func        func(testOption) int
+	NeedGlide   bool
+	NeedYarn    bool
 	Description string
 }
 
 var testCommands = []*testCommandInfo{
 	&testCommandInfo{
-		Command: "sunit",
-		Func: runGoUnitTest,
-		NeedGlide: true,
-		NeedYarn: false,
+		Command:     "sunit",
+		Func:        runGoUnitTest,
+		NeedGlide:   true,
+		NeedYarn:    false,
 		Description: "Run goapp test",
 	},
 	&testCommandInfo{
-		Command: "cunit",
-		Func: runAngularUnitTest,
-		NeedGlide: false,
-		NeedYarn: true,
+		Command:     "cunit",
+		Func:        runAngularUnitTest,
+		NeedGlide:   false,
+		NeedYarn:    true,
 		Description: "Run ng test",
 	},
 	&testCommandInfo{
-		Command: "e2e",
-		Func: runE2ETest,
-		NeedGlide: true,
-		NeedYarn: true,
+		Command:     "e2e",
+		Func:        runE2ETest,
+		NeedGlide:   true,
+		NeedYarn:    true,
 		Description: "Run ng e2e",
 	},
 }
@@ -59,7 +64,7 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Options:\n")
 		parser.PrintDefaults()
 		fmt.Fprintf(os.Stderr, "Commands:\n")
-		for _, c := range(testCommands){
+		for _, c := range testCommands {
 			fmt.Fprintf(os.Stderr, "  %s: %s\n", c.Command, c.Description)
 		}
 	}
@@ -67,11 +72,13 @@ func main() {
 	parser.BoolVar(&option.IsVerbose, "v", false, "Verbose output")
 	parser.BoolVar(&option.IsVerbose, "verbose", false, "Verbose output")
 	parser.BoolVar(&option.IsDocker, "docker", false, "Specify when running in docker")
+	parser.BoolVar(&option.IsCoverage, "coverage", false, "Output coverage")
+	parser.StringVar(&option.LogDir, "log", ".", "Specify the directory to output logs")
 	noSetup := parser.Bool("nosetup", false, "Skip calling glide install or yarn")
 	parser.Parse(os.Args[1:])
 	commands := parser.Args()
 	if len(commands) <= 0 {
-		for _, c := range(testCommands){
+		for _, c := range testCommands {
 			commands = append(commands, c.Command)
 		}
 	}
@@ -80,10 +87,10 @@ func main() {
 	var needYarn bool
 	commandsToRun := []*testCommandInfo{}
 	commandMap := map[string]*testCommandInfo{}
-	for _, c := range(testCommands){
+	for _, c := range testCommands {
 		commandMap[c.Command] = c
 	}
-	for _, command := range(commands) {
+	for _, command := range commands {
 		c, ok := commandMap[command]
 		if !ok {
 			fmt.Fprintf(os.Stderr, "Unknown Command: %s\n", command)
@@ -108,7 +115,7 @@ func main() {
 		}
 	}
 	if needYarn && !*noSetup {
-		func(){
+		func() {
 			popd := pushd("client")
 			defer popd()
 			if ret := runCommand("yarn"); ret != 0 {
@@ -116,7 +123,7 @@ func main() {
 			}
 		}()
 	}
-	for _, c := range(commandsToRun){
+	for _, c := range commandsToRun {
 		if ret := c.Func(option); ret != 0 {
 			os.Exit(ret)
 		}
@@ -124,7 +131,7 @@ func main() {
 	os.Exit(0)
 }
 
-func runCommand(name string, args... string) int {
+func runCommand(name string, args ...string) int {
 	fmt.Fprintf(os.Stderr, "# %v %v\n", name, strings.Join(args, " "))
 	cmd := exec.Command(name, args...)
 	cmd.Stdout = os.Stdout
@@ -167,23 +174,149 @@ func pushd(dir string) func() {
 	}
 }
 
+// カバレッジファイルの出力フォーマットは
+// mode: set
+// file.go:line.column,line.column statements count
+// file.go:line.column,line.column statements count
+// ...
+// というフォーマットになっている。
+// count >= 1 であれば実行されているということなので、
+// * 逆順ソートする
+// * 前方 2 カラムが同一であれば 2 行目移行は無視
+// という処理をする。
+type coverageSummarizer struct {
+	coverageLogs []string
+}
+
+func (coverage *coverageSummarizer) Load(path string) error {
+	var scanner *bufio.Scanner
+	if temp, err := os.Open(path); err == nil {
+		defer temp.Close()
+		scanner = bufio.NewScanner(temp)
+	} else {
+		return err
+	}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "mode:") {
+			continue
+		}
+		coverage.coverageLogs = append(coverage.coverageLogs, line)
+	}
+	return scanner.Err()
+}
+
+func (coverage *coverageSummarizer) Save(path string) error {
+	sort.Sort(sort.Reverse(sort.StringSlice(coverage.coverageLogs)))
+	var coverageFile *bufio.Writer
+	if _coverageFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644); err == nil {
+		coverageFile = bufio.NewWriter(_coverageFile)
+		defer _coverageFile.Close()
+	} else {
+		return err
+	}
+	if _, err := coverageFile.WriteString("mode: set\n"); err != nil {
+		return err
+	}
+	label := ""
+	for _, line := range coverage.coverageLogs {
+		// 範囲 行数 実行回数
+		// になっている。原則、範囲 行数 はいつも同じ値になるので、
+		// 実行回数が最大のものだけ （＝ソート順で最大のものだけ） 採用する。
+		units := strings.Split(line, " ")
+		if len(units) != 3 {
+			log.Printf("Ignore unexpected line: %v", line)
+			continue
+		}
+		currentLabel := strings.Join(units[0:2], " ")
+		if label == currentLabel {
+			continue
+		}
+		label = currentLabel
+		if _, err := coverageFile.WriteString(fmt.Sprintf("%s\n", line)); err != nil {
+			return err
+		}
+	}
+	return coverageFile.Flush()
+}
+
 func runGoUnitTest(option testOption) int {
-	args := []string{
-		"test",
+	packages := []string{
 		"./server",
 		"./testutil",
+	}
+	args := []string{
+		"test",
 	}
 	if option.IsVerbose {
 		args = append(args, "-v")
 	}
-	return runCommand("goapp", args...)
+	if !option.IsCoverage {
+		// カバレッジを使用しない場合、複数のパッケージのテストを一度に起動して良い。
+		args = append(args, packages...)
+		return runCommand("goapp", args...)
+	}
+
+	// カバレッジを使用する場合、パッケージごとに実行が必要
+	ret := 0
+	coverage := &coverageSummarizer{}
+	for idx, packageName := range packages {
+		if !func() bool {
+			tempCoverageFilePath := filepath.Join(
+				option.LogDir,
+				fmt.Sprintf("coverage_tmp_%02d.out", idx),
+			)
+			argsForCoverage := make([]string, len(args))
+			copy(argsForCoverage, args)
+			argsForCoverage = append(
+				argsForCoverage,
+				fmt.Sprintf("-coverprofile=%s", tempCoverageFilePath),
+				fmt.Sprintf("-coverpkg=%s", strings.Join(packages, ",")),
+				packageName,
+			)
+			ret = runCommand("goapp", argsForCoverage...)
+			if ret != 0 {
+				return false
+			}
+			defer os.Remove(tempCoverageFilePath)
+
+			if err := coverage.Load(tempCoverageFilePath); err != nil {
+				log.Printf("ERROR: Failed to read from %v: %v", tempCoverageFilePath, err)
+				ret = 1
+				return false
+			}
+
+			return true
+		}() {
+			break
+		}
+	}
+
+	coverageFilePath := filepath.Join(option.LogDir, "coverage.out")
+	htmlFilePath := filepath.Join(option.LogDir, "coverage.html")
+	if err := coverage.Save(coverageFilePath); err != nil {
+		log.Printf("ERROR: Failed to write: %v: %v", coverageFilePath, err)
+		ret = 1
+	} else {
+		if htmlRet := runCommand(
+			"goapp",
+			"tool",
+			"cover",
+			fmt.Sprintf("-html=%s", coverageFilePath),
+			fmt.Sprintf("-o=%s", htmlFilePath),
+		); htmlRet != 0 {
+			log.Printf("ERROR: Failed to output: %v", htmlFilePath)
+			ret = 1
+		}
+	}
+	return ret
 }
 
 func runAngularUnitTest(option testOption) int {
 	popd := pushd("client")
 	defer popd()
 
-	args := []string {
+	args := []string{
 		"run",
 		"test",
 		"--",
@@ -199,7 +332,7 @@ func runAngularUnitTest(option testOption) int {
 
 func runE2ETest(option testOption) int {
 	serverStdout, err := os.OpenFile(
-		"server.stdout.log",
+		filepath.Join(option.LogDir, "server.stdout.log"),
 		os.O_RDWR|os.O_CREATE|os.O_APPEND,
 		0644,
 	)
@@ -208,7 +341,7 @@ func runE2ETest(option testOption) int {
 	}
 	defer serverStdout.Close()
 	serverStderr, err := os.OpenFile(
-		"server.stderr.log",
+		filepath.Join(option.LogDir, "server.stderr.log"),
 		os.O_RDWR|os.O_CREATE|os.O_APPEND,
 		0644,
 	)
@@ -225,7 +358,7 @@ func runE2ETest(option testOption) int {
 	if err != nil {
 		panic(err)
 	}
-	args := []string {
+	args := []string{
 		devAppServer,
 		"--require_indexes=true",
 		"--enable_watching_go_path=false",
@@ -250,7 +383,7 @@ func runE2ETest(option testOption) int {
 	seconds := 30
 	fmt.Fprintf(os.Stderr, "Wait for server gets ready for %v seconds: %v\n", seconds, url)
 	limit := time.Now().Add(time.Duration(seconds) * time.Second)
-	client := &http.Client {
+	client := &http.Client{
 		Timeout: time.Duration(seconds) * time.Second,
 	}
 	for true {
@@ -266,10 +399,10 @@ func runE2ETest(option testOption) int {
 	popd := pushd("client")
 	defer popd()
 
-	e2eArgs := []string {
+	e2eArgs := []string{
 		"run",
 		"e2e",
-		"--", 
+		"--",
 		"--serve",
 		"true",
 		"--port",
